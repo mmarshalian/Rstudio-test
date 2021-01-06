ecf3847d502a669b677c6402ba39ebab4d595d22
your_username <mmarshalian@gmail.com>
2021-01-05 21:35:20 -0800

    first try. intial testing

---
+++ Rstudio and git test.R
@@ -0,0 +1,6 @@
+version 
+setwd("~/Desktop/R tutorials and practice/Data scientist's toolkit/Rstudio-test")
+
+print("This file was created within RStudio")
+
+print("to check for version control on github.")
