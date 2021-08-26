5a070c2daebaeba6facac49a125ed2a0a4a69348
PAK <pkeutelian@users.noreply.github.com>
2021-08-25 21:16:09 -0700

    Add auto-check for remote packages.

---
+++ Ookla.R
@@ -0,0 +1,16 @@
+
+
+# Check if packages are installed:
+
+packages = c("remotes")
+
+package.check <- lapply(
+	packages,
+	FUN = function(x) {
+		if (!require(x, character.only = TRUE)) {
+			install.packages(x, dependencies = TRUE)
+			library(x, character.only = TRUE)
+		}
+	}
+)
+
